5e9a2a099796c60c0433f56c175de2f0eba3641e
icetee4eva <prablordeppey@gmail.com>
2022-02-16 04:53:19 +0800

    som1A done

--- our_som1A.py
+++ our_som1A.py
@@ -49,48 +49,74 @@ class SOM:
         self.initialize()
 
     def initialize(self):
+        """sets up new grid of network grid weights for optimization.
+        """
         self.net = np.random.random((self.network_dimensions[0], self.network_dimensions[1], self.num_features))
 
+
     def set_weights(self, weights):
+        """sets weight to use for SOM
+
+        Args:
+            weights (ndarray): network grid of weights for neurons.
+        """
         self.net = weights
 
+
     def train(self, data, lr_decay_function, num_epochs=1, init_learning_rate=0.01, resetWeights=False, show_plot=False, method='euler'):
+        """wrapper for training SOM using euler or runge-kutta method
+
+        Args:
+            data (ndarray): matrix of samples as rows
+            lr_decay_function (str): learning rate decay function
+            num_epochs (int, optional): total number of epochs. Defaults to 1.
+            init_learning_rate (float, optional): initial learning rate. Defaults to 0.01.
+            resetWeights (bool, optional): whether to generate new weights or use existing. Defaults to False.
+            show_plot (bool, optional): whether to show plots or not. Defaults to False.
+            method (str, optional): optimization method. euler or runge-kutta. Defaults to 'euler'.
+        """
+        self.lr_decay_function = lr_decay_function
+        self.num_epochs = num_epochs
+        self.init_learning_rate = init_learning_rate
+        self.resetWeights = resetWeights
+        self.show_plot = show_plot
+        self.data = data
+
         if method == 'runge-kutta':
-           self.runge_kutta(data, lr_decay_function, num_epochs, init_learning_rate, resetWeights=False, show_plot=False)
-        else:
-            self.euler_method(data, lr_decay_function, num_epochs, init_learning_rate, resetWeights=False, show_plot=False)
+           self.runge_kutta()
+        else:  # euler method
+            self.euler()
 
 
-    def euler_method(self, data, lr_decay_function, num_epochs=100, init_learning_rate=0.01, resetWeights=False, show_plot=False):
+    def euler(self):
         """
-        :param data: the data to be trained
-        :param num_epochs: number of epochs (default: 100)
-        :param init_learning_rate: initial learning rate (default: 0.01)
-        :param lr_decay_function: function used to decay/reduce learning rate (default: normal)
-        :param show_plot: whether to show SOM grid or not. Introduced for efficiency
-        :return:
+        weight updates in self.net using the euler equation
+            - w_n+1 = wn + a*h(x - wn)
+
+        weights update in self.approx_net using the approximation formular (solution to the ode)
+            - wn+1 = (wn + wn*x)/(1 + x), 
         """
-        if resetWeights:
+        # reset weight if the need be
+        if self.resetWeights:
             self.initialize()
-        num_rows = data.shape[0]
+        num_rows = self.data.shape[0]
         indices = np.arange(num_rows)
-        self.time_constant = num_epochs / np.log(self.init_radius)
-
-        self.approx_net =self.net.copy()
+        self.time_constant = self.num_epochs/np.log(self.init_radius)
+        self.approx_net = self.net.copy()  # w0 for approximation
 
         # visualization
-        if show_plot:
+        if self.show_plot:
             fig = plt.figure()
         else:
             fig = None
 
         # for (epoch = 1,..., Nepochs)
-        for i in range(1, num_epochs + 1):
+        for i in range(1, self.num_epochs + 1):
             # interpolate new values for α(t) and σ (t)
             radius = self.decay_radius(i)
-            learning_rate = self.decay_learning_rate(init_learning_rate, i, num_epochs, lr_decay_function)
+            learning_rate = self.decay_learning_rate(iteration=i)
             # visualization
-            #vis_interval = int(num_epochs/10)
+            #vis_interval = int(self.num_epochs/10)
             # if i % vis_interval == 0:
             #     if fig is not None:
             #         self.show_plot(fig, i/vis_interval, i)
@@ -104,7 +130,7 @@ class SOM:
 
             # for (record = 1,..., Nrecords)
             for record in indices:
-                row_t = data[record, :]
+                row_t = self.data[record, :]
 
                 # find its Best Matching Unit
                 bmu, bmu_idx = self.find_bmu(row_t)
@@ -113,9 +139,10 @@ class SOM:
                 # for (k = 1,..., K)
                 for x in range(self.network_dimensions[0]):
                     for y in range(self.network_dimensions[1]):
+
+                        # weight update for euler equation
                         weight = self.net[x, y, :].reshape(1, self.num_features)
                         w_dist = np.sum((np.array([x, y]) - bmu_idx) ** 2)
-                        
                         # if the distance is within the current neighbourhood radius
                         if w_dist <= radius ** 2:
                             # update weight vectors wk using Eq. (3)
@@ -134,34 +161,35 @@ class SOM:
         if fig is not None:
             plt.show()
 
-    def runge_kutta(self, data, lr_decay_function, num_epochs=1, init_learning_rate=0.01, resetWeights=False, show_plot=False):
+    def runge_kutta(self):
         """
-        :param data: the data to be trained
-        :param num_epochs: number of epochs (default: 100)
-        :param init_learning_rate: initial learning rate (default: 0.01)
-        :param lr_decay_function: function used to decay/reduce learning rate (default: normal)
-        :param show_plot: whether to show SOM grid or not. Introduced for efficiency
-        :return:
+        weight updates in self.net using the runge-kutta second order equation
+            - w_n+1 = wn + a*h*k2
+            - k2 = h*(x+h/2 - wn+k1/2)
+            - k1 = x - wn
+
+        weight updates in self.approx_net using the runge-kutta second order approximation formular (solution to the ode)
+            - wn+1 = (wn + wn*x)/(1 + x), 
         """
-        if resetWeights:
+        # reset weight if the need be
+        if self.resetWeights:
             self.initialize()
-        num_rows = data.shape[0]
+        num_rows = self.data.shape[0]
         indices = np.arange(num_rows)
-        self.time_constant = num_epochs / np.log(self.init_radius)
+        self.time_constant = self.num_epochs / np.log(self.init_radius)
+        self.approx_net =self.net.copy()  # w0 for approximation
 
         # visualization
-        if show_plot:
+        if self.show_plot:
             fig = plt.figure()
         else:
             fig = None
 
-        self.approx_net = self.net.copy()
-
         # for (epoch = 1,..., Nepochs)
-        for i in range(1, num_epochs + 1):
+        for i in range(1, self.num_epochs + 1):
             # interpolate new values for α(t) and σ (t)
             radius = self.decay_radius(i)
-            learning_rate = self.decay_learning_rate(init_learning_rate, i, num_epochs, lr_decay_function)
+            learning_rate = self.decay_learning_rate(iteration=i)
             # visualization
             #vis_interval = int(num_epochs/10)
             # if i % vis_interval == 0:
@@ -177,7 +205,7 @@ class SOM:
 
             # for (record = 1,..., Nrecords)
             for record in indices:
-                row_t = data[record, :]
+                row_t = self.data[record, :]
 
                 # find its Best Matching Unit
                 bmu, bmu_idx = self.find_bmu(row_t)
@@ -186,17 +214,16 @@ class SOM:
                 # for (k = 1,..., K)
                 for x in range(self.network_dimensions[0]):
                     for y in range(self.network_dimensions[1]):
+
+                        # weight update for runge-kutta equation
                         weight = self.net[x, y, :].reshape(1, self.num_features)
                         w_dist = np.sum((np.array([x, y]) - bmu_idx) ** 2)
-                        # if the distance is within the current neighbourhood radius
-                        if w_dist <= radius ** 2:
+                        if w_dist <= radius ** 2: # if the distance is within the current neighbourhood radius
                             influence = SOM.calculate_influence(w_dist, radius)
-
-                            # update weight vectors wk using Eq. (3) - runge-kutta method order 4
-                            step = influence
-                            k1 = step * (row_t - weight)  # f(x0,y0) = hf(x0,y0)
-                            k2 = (row_t+step/2 - weight+k1/2)  # h f [ xo + h/2 , yn + k1 / 2] eqn (2)
-                            new_w = weight + learning_rate * step * k2  # y_n+1 = yn + k2
+                            # update weight vectors wk using runge-kutta equation of order 4
+                            k1 = influence * (row_t - weight)  # f(x0,y0) = hf(x0,y0)
+                            k2 = (row_t+influence/2 - weight+k1/2)  # h f [ xo + h/2 , yn + k1 / 2] eqn (2)
+                            new_w = weight + learning_rate * influence * k2  # y_n+1 = yn + h*k2
                             self.net[x, y, :] = new_w.reshape(1, self.num_features)
 
                         # weight-update using rk approximation function 
@@ -206,9 +233,40 @@ class SOM:
                             w0 = self.approx_net[x, y, :].reshape(1, self.num_features)
                             dw = (w0 + w0*x)/(1+x)
                             self.approx_net[x, y, :] = dw.reshape(1, self.num_features)
-                    
+
         if fig is not None:
             plt.show()    
+
+
+    def decay_radius(self, iteration):
+        """radius within which to consider neigboiuring neurons
+
+        Args:
+            iteration (int): current iteration
+
+        Returns:
+            float: computed radius. decays with every iteration.
+        """
+        return self.init_radius * np.exp(-iteration/self.time_constant)#rk-rx^2
+
+
+    def decay_learning_rate(self, iteration):
+        """decay learning rate wrt the decay function
+
+        Args:
+            iteration (int): current iteration
+
+        Returns:
+            float: computed learning rate
+        """
+        if self.lr_decay_function == "linear":
+            return self.init_learning_rate*(1/iteration)
+        elif self.lr_decay_function == "inverse":
+            return self.init_learning_rate*(1-iteration/self.num_epochs)
+        elif self.lr_decay_function == "power":
+            return self.init_learning_rate * np.exp(-iteration/self.num_epochs)
+        return self.init_learning_rate * np.exp(iteration/self.num_epochs)  # for default or undefined
+
 
     @staticmethod
     def calculate_influence(distance, radius): #distribution
@@ -246,30 +304,6 @@ class SOM:
         """
         bmu, bmu_idx = self.find_bmu(data)
         return bmu, bmu_idx
-
-    def decay_radius(self, iteration):
-        return self.init_radius * np.exp(-iteration / self.time_constant)#rk-rx^2
-
-    def decay_learning_rate(self, initial_learning_rate, iteration, num_iterations, lr_decay_function):
-        """reduce learning rate wrt the decay function
-
-        Args:
-            initial_learning_rate (float): base learning rate
-            iteration (int): current iteration
-            num_iterations (int): total number of epochs
-            lr_decay_function (str): the function used to deacay lr
-
-        Returns:
-            float: computed learning rate
-        """
-        if lr_decay_function == "linear":
-            return initial_learning_rate*(1/iteration)
-        elif lr_decay_function == "inverse":
-            return initial_learning_rate*(1-iteration/num_iterations)
-        elif lr_decay_function == "power":
-            return initial_learning_rate * np.exp(-iteration / num_iterations)
-        # for default or undefined
-        return initial_learning_rate * np.exp(iteration / num_iterations)
 
     def show_plot(self, fig, position, epoch):
         # setup axes
